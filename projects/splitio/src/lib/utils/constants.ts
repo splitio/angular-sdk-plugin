a0aa5475c2c0c025694b8352e5b62e6b03f4b1ec
Emmanuel Zamora <emmanuel.zamora@harness.io>
2025-07-29 12:59:24 -0300

    update version and changes file

--- projects/splitio/src/lib/utils/constants.ts
+++ projects/splitio/src/lib/utils/constants.ts
@@ -1,7 +1,7 @@
 /**
  * Angular utilities version number
  */
-const ANGULAR_SDK_VERSION_NUMBER = '3.0.0';
+const ANGULAR_SDK_VERSION_NUMBER = '3.0.1-rc.0';
 /**
  * SDK constant for control treatment
  */
